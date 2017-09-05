d46a6089a4f6092a0ddfc55c2e3928b987f48b63
Dylan Ross Ludwick <dylan@devhominid.com>
2017-09-05 15:48:22 -0600

    add Trip component

---
+++ client/src/components/Trip.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import TripAPI from '../TripAPI';
+import { Link } from 'react-router-dom';
+
+const Trip = (props) => {
+  const trip = TripAPI.get(
+    parseInt(props.match.params.number, 10)
+  );
+  if (!trip) {
+    return <div>Sorry, but the trip was not found</div>
+  }
+
+  return (
+    <div>
+      <h1>{trip.title}</h1>
+      <h2>{trip.date}</h2>
+      <img src={trip.img} alt='trip image'/>
+      <h4>{trip.location}</h4>
+      <h4>{trip.type}</h4>
+      <p>{trip.notes}</p>
+      <Link to='/trips'>Back</Link>
+    </div>
+  )
+};
+
+export default Trip;
