e13dd7d90de9ef97556dd279d41a89cb950c401d
Dylan Ross Ludwick <dylan@devhominid.com>
2017-09-05 15:47:21 -0600

    add Home component

---
+++ client/src/components/Home.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import TripAPI from '../TripAPI';
+import { Link } from 'react-router-dom';
+import Background from '../images/index-cover.jpg';
+
+const coverStyle = {
+  width: '100vw',
+  height: '90vh',
+  backgroundImage: "url(" + Background + ")",
+  backgroundSize: 'cover',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center'
+};
+
+const divStyle = {
+  width: '60%',
+  background: 'rgba(216, 216, 216, 0.9)',
+  paddingBottom: '20px',
+}
+
+const headerStyle = {
+  marginLeft: '5%',
+  marginBottom: '7px'
+}
+
+const rowStyle = {
+  width: '90%',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between'
+}
+
+const imgStyle = {
+  width: '200px',
+  height: '175px'
+}
+
+const Home = () => (
+  <div style={coverStyle}>
+    <div style={divStyle}>
+      <h2 style={headerStyle}>Recent Trips</h2>
+      <div style={rowStyle}>
+        {
+          TripAPI.getRecent().map(t => (
+            <Link to={`/trips/${t.number}`} key={t.number}>
+              <img src={t.img} alt='trip' style={imgStyle} />
+            </Link>
+          ))
+        }
+      </div>
+      <h2 style={headerStyle}>Future Trips</h2>
+      <div style={rowStyle}>
+        {
+          TripAPI.getFuture().map(t => (
+            <Link to={`/trips/${t.number}`} key={t.number}>
+              <img src={t.img} alt='trip' style={imgStyle} />
+            </Link>
+          ))
+        }
+      </div>
+    </div>
+  </div>
+);
+
+export default Home;
