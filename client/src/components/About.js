89ec660a3ec8f0c86175992505501cc7ab71b7d1
Dylan Ross Ludwick <dylan@devhominid.com>
2017-09-05 15:42:57 -0600

    add About component

---
+++ client/src/components/About.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras viverra quam non
+      viverra tempor. Mauris sed euismod risus. Integer a commodo elit. Vestibulum aliquam
+      magna fermentum ligula semper lacinia. Ut faucibus leo ut nisi tempor, hendrerit
+      elementum sem tristique. Mauris metus elit, vehicula vel condimentum ultrices,
+      tincidunt et turpis. Cras auctor dapibus nulla.
+    </p>
+    <p>
+      Cras eu justo condimentum mauris ultricies faucibus. Morbi ut ullamcorper velit. Nulla
+      ac interdum nunc. Donec id posuere nisi, id sagittis odio. Proin sodales turpis eget
+      velit congue, ut cursus mauris convallis. Pellentesque hendrerit, orci vel accumsan
+      accumsan, libero tellus ornare mauris, feugiat ultrices quam leo a massa. Nulla
+      facilisi. Praesent ac dictum lacus. Morbi in dolor a turpis ullamcorper placerat vitae
+      in purus. Sed pharetra non magna eu scelerisque. Ut in tortor in velit consectetur
+      sollicitudin. Cras non nibh quis nisi pharetra tincidunt. Praesent in lorem in enim
+      aliquam finibus. Nulla malesuada erat sit amet efficitur consequat.
+    </p>
+
+  </div>
+);
+
+export default About;
