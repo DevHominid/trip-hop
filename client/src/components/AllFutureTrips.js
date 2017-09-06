545c717fe35cc2d03308bc89696e40a8e85ece0c
Dylan Ross Ludwick <dylan@devhominid.com>
2017-09-06 14:19:59 -0600

    build and style components

--- client/src/components/AllFutureTrips.js
+++ client/src/components/AllFutureTrips.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import Trip from './Trip';
+import TripAPI from '../TripAPI';
+import { Link } from 'react-router-dom';
 
 const AllFutureTrips = () => (
   <div>
-    <h1>All Future Trips</h1>
+    <section>
+      <div style={styles.divBlock}>
+        {
+          TripAPI.getFuture().map(t => (
+            <Link to={`/trips/${t.status}/${t.number}`} key={t.number} style={styles.linkBlock}>
+              <img src={t.img} alt='trip' style={styles.img} />
+            </Link>
+          ))
+        }
+      </div>
+    </section>
   </div>
 );
+
+const styles ={
+  divBlock: {
+    width: '90%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-evenly'
+  },
+  linkBlock: {
+    margin: '20px'
+  },
+  img: {
+    width: '225px',
+    height: '175px'
+  }
+}
 
 export default AllFutureTrips;
