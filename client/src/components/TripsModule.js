dbb03e48e29d574b420ddae92aa1dcedf7482e68
Dylan Ross Ludwick <dylan@devhominid.com>
2017-09-07 12:31:08 -0600

    update styles

--- client/src/components/TripsModule.js
+++ client/src/components/TripsModule.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TripAPI from '../TripAPI';
 import { Link } from 'react-router-dom';
+import Background from '../images/topo-map2.png';
 
 const TripsModule = () => (
   <div style={styles.divBlock}>
@@ -29,24 +30,29 @@ const TripsModule = () => (
 
 const styles = {
   divBlock: {
-    width: '80%',
-    paddingBottom: '20px',
+    width: '95vw',
+    background: "linear-gradient(0deg, rgba(255,255,255,0.0), rgba(255,255,255,0.0)), url(" + Background + ")",
+    paddingBottom: '40px',
+    paddingTop: '20px',
+    marginBottom: '20px',
+    marginTop: '20px',
     marginLeft: 'auto',
     marginRight: 'auto',
   },
   heading: {
-    width: '50%',
+    width: '33.3%',
     textAlign: 'center',
-    padding: '15px 10px',
-    margin: '50px auto 30px auto',
+    padding: '10px 15px',
+    margin: '20px auto 30px auto',
     fontFamily: 'Maven Pro, sans-serif',
     fontWeight: 'normal',
-    border: '1px solid #0A0908'
+    borderBottom: '1px solid #fff',
+    color: '#fff'
   },
   row: {
     display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-around'
   },
   img: {
     width: '200px',
