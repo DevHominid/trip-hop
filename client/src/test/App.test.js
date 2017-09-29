755cce1821bfda86305cd7170b61a6ff0f9e8828
Dylan Ross Ludwick <dylan@devhominid.com>
2017-09-29 14:08:57 -0600

    write app comp unit tests

--- client/src/test/App.test.js
+++ client/src/test/App.test.js
@@ -3,10 +3,22 @@ import { expect } from 'chai';
 import { shallow } from 'enzyme';
 
 import App from '../components/App';
+import Header from '../components/Header';
+import Main from '../components/Main';
 
 describe('<App />', () => {
   it('renders without imploding', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.length).to.equal(1);
+  });
+
+  it('renders one <Header /> component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Header)).to.have.length(1);
+  });
+
+  it('renders one <Main /> component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Main)).to.have.length(1);
   });
 });
