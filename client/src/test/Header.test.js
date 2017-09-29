2577add0df111afc9d76b5951ed0fc54d10ddcc1
Dylan Ross Ludwick <dylan@devhominid.com>
2017-09-29 14:15:38 -0600

    update Header unit tests

--- client/src/test/Header.test.js
+++ client/src/test/Header.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
+import { Link } from 'react-router-dom';
 
 import Header from '../components/Header';
 
@@ -9,4 +10,9 @@ describe('<Header />', () => {
     const wrapper = shallow(<Header />);
     expect(wrapper.length).to.equal(1);
   });
+
+  it('renders three <Link /> components', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find(Link)).to.have.length(3);
+  })
 });
